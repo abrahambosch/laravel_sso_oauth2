2ac7260af7f45726219c38d5e78d3bd351cb4e53
Abraham Bosch <abrahambosch@gmail.com>
2016-10-27 16:48:51 +0000

    cleaned up the routes/web.php file

--- routes/web.php
+++ routes/web.php
@@ -29,47 +29,14 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
+// socialite routes
+Route::get('auth/{provider}', 'Auth\SocialAuthController@redirectToProvider')->name("socialite.redirect");
+Route::get('auth/{provider}/callback', 'Auth\SocialAuthController@handleProviderCallback')->name("socialite.callback");
 
 
-// oauth client routes
-
-// First route that user visits on consumer app
-Route::get('/single-signon', function () {
-    // Build the query parameter string to pass auth information to our request
-    $query = http_build_query([
-        'client_id' => 1,
-        'redirect_uri' => 'http://oauthconsumer.dev/oauth/zerotouch/callback',
-        'response_type' => 'code',
-        'scope' => ''
-    ]);
-
-    // Redirect the user to the OAuth authorization page
-    return redirect('http://local.api.zerotouch.live/oauth/authorize?' . $query);
-});
-
-// Route that user is forwarded back to after approving on server
-Route::get('oauth_callback', function (Request $request) {
-    $http = new GuzzleHttp\Client;
-
-    //$vars = $request->all();
-    //dd($vars);
-
-    $response = $http->post('http://passport.dev/oauth/token', [
-        'form_params' => [
-            'grant_type' => 'authorization_code',
-            'client_id' => 3, // from admin panel above
-            'client_secret' => 'doXf9lndKwIr1hLJRFjp9UnOk5ZRTcyJlxmGi5kL', // from admin panel above
-            'redirect_uri' => 'http://oauthconsumer.dev/oauth_callback',
-            'code' => $request->input("code") // Get code from the callback
-        ]
-    ]);
-
-    // echo the access token; normally we would save this in the DB
-    //return json_decode((string) $response->getBody(), true)['access_token'];
-    return json_decode((string) $response->getBody(), true);
-});
-
-
+/**
+ * example of how to get a token for an authenticated user.
+ */
 Route::get('/jwt', function (Request $request) {
     //$user = $request->user();
     $user = Auth::user();
@@ -79,6 +46,10 @@ Route::get('/jwt', function (Request $request) {
     return Response::json(compact('token'));
 })->middleware('auth');
 
+
+/**
+ * test endpoint that is restricted by JWT.
+ */
 Route::get('/restricted', [
     'before' => 'jwt-auth',
     function () {
@@ -96,9 +67,6 @@ Route::get('/restricted', [
 
 
 
-// socialite routes
-// todo: make this generic so provider is specified in the url
-Route::get('auth/{provider}', 'Auth\SocialAuthController@redirectToProvider')->name("socialite.redirect");
-Route::get('auth/{provider}/callback', 'Auth\SocialAuthController@handleProviderCallback')->name("socialite.callback");
+
 
 
